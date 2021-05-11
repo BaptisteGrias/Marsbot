ad7d2c6bc51907972b0b1161b76075a4259e6e31
TouyardK <touyard.kevin@outlook.fr>
2021-05-11 23:40:36 +0200

    Add Picture Page

---
+++ marsbot/src/components/PicturesCard.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import PicturesList from './PicturesList';
+
+const PicturesCard = ({ img_src, earth_date, id }) => (
+  <>
+    <h2 className="titleCard">{earth_date}</h2>
+    <div className="centerBox">
+      <img className="picture" src={img_src} alt={img_src} width={400} height={350}></img>
+    </div>
+
+    <p className="CardID">{id}</p>
+  </>
+);
+
+export default PicturesCard;
