5748b58362278555f0eaf6dfd3888829f658806b
Baptiste Grias <baptistegrias@new-host-3.home>
2021-05-12 02:24:27 +0200

    creation de la homepage + navbar

--- marsbot/src/App.js
+++ marsbot/src/App.js
@@ -2,10 +2,12 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import ChatBot from './components/ChatBot';
 //import TestBot from './components/TestBot';
+import HomePage from './components/Homepage';
 
 function App() {
   return (
-    <div className='App'>
+    <div className="App">
+      <HomePage mars="./mars.png" />
       <ChatBot />
     </div>
   );
